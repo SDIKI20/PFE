56eb9d49127bf9fb2e1c1e80e55a7dd1a8a46ea4
odaydid002 <odaydid002@gmail.com>
2025-02-24 17:20:46 +0100

    Update car details and enhance dark mode support; swap Audi and Hyundai images, improve dark mode styles, and add transition effects for smoother UI

--- js/darkmode.js
+++ js/darkmode.js
@@ -21,9 +21,20 @@ function toggleDark(){
     root.style.setProperty('--txt-white', 'black');
     root.style.setProperty('--text', '#d3d0d0');
     root.style.setProperty('--bg-darker', '#1F2936');
+    root.style.setProperty('--border', 'rgba(129, 129, 129, 0.247)');
 }
 
 function toggleLight(){
-    root.style.setProperty('--white', '#f0eded');
+    try{
+        document.getElementById("navLogo").style.filter = "invert(0)"
+        document.getElementById("navLogoFoot").style.filter = "invert(0)"
+    }catch(error){}
+    root.style.setProperty('--bg', '#f0eded');
+    root.style.setProperty('--dark-bg', '#1a1a1a');
+    root.style.setProperty('--container', 'white');
+    root.style.setProperty('--txt-black', 'black');
+    root.style.setProperty('--txt-white', 'white');
+    root.style.setProperty('--text', '#333');
+    root.style.setProperty('--bg-darker', '#d3d0d0');
 }
 
