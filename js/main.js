af3a682382e8f8b2f3504a6f7e15342d3b5226da
odaydid002 <odaydid002@gmail.com>
2025-02-25 18:49:07 +0100

    added notification function to main.js

--- js/main.js
+++ js/main.js
@@ -45,3 +45,49 @@ try{
         window.scrollTo(0, 0);
     })
 }catch(error){}
+
+const notificationContainer = document.createElement('div')
+notificationContainer.id = "notifContainer"
+notificationContainer.classList.add("notifications-container", "flex-col")
+document.body.appendChild(notificationContainer)
+
+function pushNotif(type, message){
+    const notif = document.createElement("div")
+    notif.classList.add("notification-container", "flex-row")
+    notifIcon = ""
+    switch (type){
+        case "w":
+            notif.classList.add("notif-warning")
+            notifIcon = "fa-circle-xmark"
+        break;
+        case "e":
+            notif.classList.add("notif-error")
+            notifIcon = "fa-triangle-exclamation"
+        break;
+        case "s":
+            notif.classList.add("notif-success")
+            notifIcon = "fa-circle-check"
+            break;
+        case "i":
+            notif.classList.add("notif-info")
+            notifIcon = "fa-circle-exclamation"
+        break;
+    }
+    notif.innerHTML = `
+        <i class="fa-solid ${notifIcon} notif-type"></i>
+        <p class="notif-msg">${message}</p>
+    `
+    closeBut = document.createElement('i')
+    closeBut.classList.add("fa-solid", "fa-xmark", "notif-close", "bt-hover")
+    closeBut.addEventListener('click', ()=>{
+        closeBut.parentElement.parentElement.removeChild(notif)
+    })
+    notif.appendChild(closeBut)
+    try{
+        document.getElementById("notifContainer").appendChild(notif)
+        setTimeout(() => {
+            notif.style.opacity = "1"
+            notif.style.transform = "translateY(0)"
+        }, 100);
+    }catch(error){}
+}
