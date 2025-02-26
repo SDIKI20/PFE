93b65635b0e1486413219126c24d4b48d655b50f
SMAIL <ismailsdiki5@gmail.com>
2025-02-26 23:26:20 +0100

    refactor users table: update schema with new fields and constraints for improved data management

--- projet_fin_etude.sql
+++ projet_fin_etude.sql
@@ -1,14 +1,21 @@
 -- Users Table
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    last_name VARCHAR(50) NOT NULL,
-    first_name VARCHAR(50) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    phone VARCHAR(20) UNIQUE NOT NULL,
-    password TEXT NOT NULL,
-    role VARCHAR(10) CHECK (role IN ('admin', 'employee', 'client')) NOT NULL,
-    creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+ CREATE TABLE users (
+            id SERIAL PRIMARY KEY,
+            email VARCHAR(255) UNIQUE NOT NULL,
+            username VARCHAR(255) NOT NULL,
+            password VARCHAR(255) NOT NULL,
+            fname VARCHAR(20) NOT NULL,
+            lname VARCHAR(20) NOT NULL,
+            address VARCHAR(255) NOT NULL,
+            country VARCHAR(255) NOT NULL,
+            wilaya VARCHAR(255) NOT NULL,
+            city VARCHAR(255) NOT NULL,
+            zipcode VARCHAR(10) NOT NULL,
+            image VARCHAR(255) NOT NULL DEFAULT '../assets/images/user.jpg' ,
+            phone VARCHAR(15) NOT NULL,
+            birthdate DATE NOT NULL,
+            role VARCHAR(50) NOT NULL CHECK (role IN ('admin', 'employee', 'client'))
+        );
 
 -- Vehicles Table
 CREATE TABLE vehicles (
